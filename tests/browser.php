ca032ba0ba5b73e7ea29562e957d04fc2c5c6b75
Marijn Ophorst <marijn@monomelodies.nl>
2019-04-14 15:53:43 +0200

    setup tests

---
+++ tests/browser.php
@@ -0,0 +1,21 @@
+<?php
+
+use Gentry\Gentry\Wrapper;
+
+/** Testsuite for Toast\Acceptance\Browser */
+return function () : Generator {
+    $object = Wrapper::createObject(Toast\Acceptance\Browser::class);
+    /** get yields true */
+    yield function () use ($object) {
+        $result = $object->get('MIXED');
+        assert(true);
+    };
+
+    /** post yields true */
+    yield function () use ($object) {
+        $result = $object->post('MIXED', []);
+        assert(true);
+    };
+
+};
+
