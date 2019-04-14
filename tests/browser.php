c1e0abb5afc8916def416b3c58b1837e02e2d9ae
Marijn Ophorst <marijn@monomelodies.nl>
2019-04-14 17:04:06 +0200

    at least one working test, other one needs thought...

--- tests/browser.php
+++ tests/browser.php
@@ -7,13 +7,14 @@ return function () : Generator {
     $object = Wrapper::createObject(Toast\Acceptance\Browser::class, 'chromium');
     /** get yields true */
     yield function () use ($object) {
-        $result = $object->get('MIXED');
-        assert(true);
+        $page = $object->get('file://'.__DIR__.'/files/get.html');
+        $value = $page->evaluate('document.querySelector("body").innerHTML')->getReturnValue();
+        assert(trim($value) === '<h1>Hello world</h1>');
     };
 
     /** post yields true */
     yield function () use ($object) {
-        $result = $object->post('MIXED', []);
+        $result = $object->post('file://'.__DIR__.'/files/post.html', 'form', ['foo' => 'bar']);
         assert(true);
     };
 
