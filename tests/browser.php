b6583de3059b97c7935ffb21eec0eab9ad1f5a86
Marijn Ophorst <marijn@sensimedia.nl>
2019-04-15 11:33:28 +0200

    rename some stuff, and correctly test posting

--- tests/browser.php
+++ tests/browser.php
@@ -4,18 +4,19 @@ use Gentry\Gentry\Wrapper;
 
 /** Testsuite for Toast\Acceptance\Browser */
 return function () : Generator {
-    $object = Wrapper::createObject(Toast\Acceptance\Browser::class, 'chromium');
+    $browser = Wrapper::createObject(Toast\Acceptance\Browser::class, 'chromium');
     /** get yields true */
-    yield function () use ($object) {
-        $page = $object->get('file://'.__DIR__.'/files/get.html');
+    yield function () use ($browser) {
+        $page = $browser->get('file://'.__DIR__.'/files/get.html');
         $value = $page->evaluate('document.querySelector("body").innerHTML')->getReturnValue();
         assert(trim($value) === '<h1>Hello world</h1>');
     };
 
     /** post yields true */
-    yield function () use ($object) {
-        $result = $object->post('http://localhost:8000', 'form', ['foo' => 'bar']);
-        assert(true);
+    yield function () use ($browser) {
+        $page = $browser->post('http://localhost:8000', 'form', ['foo' => 'bar']);
+        $value = $page->evaluate('document.querySelector("h1").innerHTML')->getReturnValue();
+        assert($value == 'bar');
     };
 
 };
